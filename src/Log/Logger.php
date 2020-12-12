293539410275d6ce9d61be5f132fe9ac7125ceef
Alex Dontsov <dcorpsoftware@gmail.com>
2020-12-12 19:57:12 +0700

    fix

--- src/Log/Logger.php
+++ src/Log/Logger.php
@@ -1,7 +1,17 @@
 <?php
+namespace Log;
 
-
+/**
+ * Class Logger
+ * @package Log
+ */
 class Logger
 {
-
+    /**
+     * @return void
+     */
+    public function run() : void
+    {
+        echo 'Hello Logger!';
+    }
 }
